30031efabdc8ada9e2af84eefe3858e20e49e470
jinseok-nam <jinseok.n@gmail.com>
2014-01-06 19:27:19 +0100

    Fixed errors in install.sh

--- install.sh
+++ install.sh
@@ -9,18 +9,18 @@ wget https://www.cise.ufl.edu/research/sparse/CSparse/CSparse.tar.gz
 tar xvzf CSparse.tar.gz
 cd CSparse
 make library
-cp Lib/libcsparse.a ../../lib/
-cp Include/cs.h ../../include/
+cp Lib/libcsparse.a ../lib/
+cp Include/cs.h ../include/
 
 ###################
 # Install OpenBLAS
 ###################
 
 # remove binary if installed
-sudo apt-get remove libopenblas-base
+#sudo apt-get remove libopenblas-base
 
-cd /tmp/
-git clone https://github.com/xianyi/OpenBLAS.git
-cd OpenBLAS
-make NO_AFFINITY=1 USE_OPENMP=1
-sudo make install
+#cd /tmp/
+#git clone https://github.com/xianyi/OpenBLAS.git
+#cd OpenBLAS
+#make NO_AFFINITY=1 USE_OPENMP=1
+#sudo make install
